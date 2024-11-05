422b6f9eda6f8374873bda6f9ad7ec492b8b889c
amanak7 <22je0094@iitism.ac.in>
2024-11-05 20:59:39 +0530

    home page

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -2,8 +2,9 @@ import React from 'react'
 
 function About() {
   return (
-    <div>
-      About Page
+    <div className='flex justify-center align-middle w-[100vw] h-[100vh] items-center'>
+    <h1>
+    About Page</h1>
     </div>
   )
 }
